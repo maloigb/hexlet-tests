809cf7a888fa3debc4122f737213d1b6e80f55fa
Daniil Siamenka <maloigb@gmail.com>
2022-12-08 21:39:04 +0300

    newInfoAssert

--- tests/capitalize.tests.js
+++ tests/capitalize.tests.js
@@ -1,11 +1,15 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js'
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
+// if (capitalize('hello') !== 'Hello') {
+//     throw new Error('Функция работает неверно!');
+//   }
   
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
+//   if (capitalize('') !== '') {
+//     throw new Error('Функция работает неверно!');
+//   }
   
-  console.log('Все тесты пройдены!');
+//   console.log('Все тесты пройдены!'); - ручное.
+
+assert.equal(capitalize(''), '');
+assert.equal(capitalize('hello'), 'Hello'); // - assert вариант использования тестов.
